cbb2fbe7514e1e3befa2e0b5e247f25fd2477fc7
Jason Cruz <badmonkey.dev@gmail.com>
2019-11-19 20:50:20 -0600

    adjusting css

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import OperatorButton from "./OperatorButton";
+import { operators } from "../../../data";
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = props => {
+  const [operatorState] = useState(operators);
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <div className='operatorButtons'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+      operatorState.map((item, index) => {
+        console.log(item);
+        return <OperatorButton setTotal={props.setTotal} key={index} operators={item.char} />;
+      })}
     </div>
   );
 };
+
+export default Operators;
